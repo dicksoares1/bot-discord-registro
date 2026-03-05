68b2b977115f77c40d78a781fa8690c87ec5b70f
dicksoares1 <dicksoares@gmail.com>
2026-03-05 11:15:31 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1186,8 +1186,7 @@ class FabricacaoView(discord.ui.View):
         task = bot.loop.create_task(acompanhar_producao(pid))
         producoes_tasks[pid] = task
         if not interaction.response.is_done():
-            await interaction.response.defer()
-
+            
         await interaction.response.defer()
 # =========================================================
 # ================= LOOP DE ACOMPANHAMENTO =================
@@ -3910,6 +3909,7 @@ async def on_ready():
 if __name__ == "__main__":
     print("🚀 Iniciando bot...")
     bot.run(TOKEN)
+
 
 
 
